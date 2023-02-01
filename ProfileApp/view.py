8a1254372514c194ee891af315f8ee3bf717949e
Natthawat18 <natthawat.to@rmuti.ac.th>
2023-02-01 18:52:39 +0700

    a

--- ProfileApp/view.py
+++ ProfileApp/view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def test(request):
@@ -21,4 +21,34 @@ def dreamJob(request):
 
 def roleModel(request):
     return render(request, 'roleModel.html')
+def product(request):
+    name = "ณัฐวัฒน์"
+    surname = "ทองคำเหลา"
+    nickname = "ปอม"
+    age = "21 ปี"
+    weight = "55"
+    height = "165" \
+             ""
+    color = "ดำ"
+    song = "ย้ายป่าหนี"
+    phoneNumber = "0627129026"
+    studenCode = "65342310140-4"
+    listNameProduct = [
+        ['มาร์คมะขามน้ำผึ้ง', 190, '../../static/images/add.jpg'],
+        ["มาร์คมะกู้ด", 199, '../../static/images/add.jpg'],
+        ["ยาสีฟันนมแพะ", 199, '../../static/images/add.jpg'],
+        ["น้ำหอม JANUA กลิ่น flower shop", 290, '../../static/images/add.jpg'],
+        ["น้ำหอม JANUA กลิ่น wood sand and fresh vibe", 290, '../../static/images/add.jpg'],
+        ["น้ำหอม JANUA กลิ่น srxy on the beach", 290, '../../static/images/add.jpg'],
+        ["น้ำหอม JANUA กลิ่น sweetie picnic", 290, '../../static/images/add.jpg'],
+        ["กลอสดอกไม้ 01-poppy pink", 199, '../../static/images/add.jpg'],
+        ["กลอสดอกไม้ 02-sunny flower", 199, '../../static/images/add.jpg'],
+        ["กลอสดอกไม้ 03-ruby tulip", 199, '../../static/images/add.jpg']
+    ]
+    return render(request, 'product.html',
+                  {'name': name, 'surname': surname, 'nickname': nickname, 'age': age, 'wight': weight,
+                   'height': height,
+                   'color': color, 'song': song, 'phoneNumber': phoneNumber, 'studenCode': studenCode,
+                   'ListNameProduct': listNameProduct})
+
 
--- djangoProject140/urls.py
+++ djangoProject140/urls.py
@@ -4,12 +4,13 @@ from ProfileApp import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', view.home, name='home'),
+    path('', view.home, name='home'),
     path('personalRecord',view.personalRecord,name='personalRecord'),
     path('educationalRecord',view.educationalRecord,name='educationalRecord'),
     path('interests',view.interests,name='interests'),
     path('dreamJob',view.dreamJob,name='dreamJob'),
-    path('roleModel',view.roleModel,name='roleModel')
+    path('roleModel',view.roleModel,name='roleModel'),
+    path('prodcut',view.product,name='product'),
 ]
 
 
